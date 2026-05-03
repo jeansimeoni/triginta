9e07a59f783265f21041eba672f5c7b19afee943
Jean Simeoni <jeansimeoni@outlook.com>
2026-05-03 14:54:04 -0300

    release: prepare 0.1.2

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -7173,7 +7173,7 @@ mod tests {
             Span::raw(" "),
             Span::raw("[D]"),
             Span::raw("  "),
-            Span::raw("0.1.1"),
+            Span::raw("0.1.2"),
         ];
 
         assert_eq!(status_bar_spans_width(spans.as_slice()), 33);
